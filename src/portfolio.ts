424db848dc19fd5c6fa8ececaae78f9f63ae72c1
Arjun Kannan <arj.shiv@gmail.com>
2016-01-08 22:16:24 -0500

    First few lines of code

--- src/portfolio.ts
+++ src/portfolio.ts
@@ -1,7 +1,8 @@
 class Portfolio {
   name: String;
   positions: Array<Holding>;
-  constructor(args: Portfolio | Array<Holding> | constructorArgs) {
+
+  constructor(args: Portfolio | Array<Holding> | ConstructorArgs | void) {
     this.name = 'Portfolio ' + Math.floor(Date.now() / 1000);
     if (args instanceof Portfolio) {
       this.name = args.name;
@@ -9,7 +10,7 @@ class Portfolio {
     } else if (args instanceof Array) {
       this.setPositions(args);
     } else {
-
+      console.log();
     }
   }
 
@@ -17,20 +18,20 @@ class Portfolio {
     this.positions.length = 0;
   }
 
-  setPositions(value:Array<Holding>) {
+  setPositions(value: Array<Holding>) {
     this.emptyPositions();
     this.positions.push.apply(this.positions, value);
   }
 
-/*  clone(other:Portfolio) {
-    var cloned:Portfolio = new Portfolio();
-    for (var key in other) {
-      if (other.hasOwnProperty(key)) {
-        cloned[key] = other[key]
-      }
-    }
-    return cloned;
-  }*/
+  /*  clone(other:Portfolio) {
+   var cloned:Portfolio = new Portfolio();
+   for (var key in other) {
+   if (other.hasOwnProperty(key)) {
+   cloned[key] = other[key]
+   }
+   }
+   return cloned;
+   }*/
 
 }
 
